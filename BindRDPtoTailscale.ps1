285ed0249abe939e8acfb9a1f719ab9609be1d7b
Brandon <brandon@willjasen.com>
2023-08-27 13:53:23 -0400

    initial commit
    changes Remote Desktop Services to only listen on the Tailscale adapter if is installed, then restarts the service

---
+++ BindRDPtoTailscale.ps1
@@ -0,0 +1,29 @@
+# Display which interface(s) that RDP is bound to
+$currentSettings = (gwmi Win32_TSNetworkAdapterSetting -filter "TerminalName='RDP-Tcp'" -namespace "root/cimv2/TerminalServices" | Select NetworkAdapterLanaID,NetworkAdapterName)
+Write-Host ("Network adapter ID for RDP: " + $currentSettings.NetworkAdapterLanaID)
+Write-Host ("Network adapter name for RDP: " + $currentSettings.NetworkAdapterName)
+
+# Attempt to bind RDP to Tailscale adapter
+$ts = gwmi Win32_TSNetworkAdapterSetting -filter "TerminalName='RDP-Tcp'" -namespace "root/cimv2/TerminalServices"
+$found = $false
+For($i=0; $i -lt ($ts.DeviceIDList).Count; $i++) {
+	If($ts.NetworkAdapterList[$i] -eq "Tailscale Tunnel") {
+		$ts.SetNetworkAdapterLanaID($ts.DeviceIDList[$i])
+		$found = $true
+		break;
+	}
+}
+
+# Display results and restart RDP service if needed
+If($found) {
+	$updatedSettings = (gwmi Win32_TSNetworkAdapterSetting -filter "TerminalName='RDP-Tcp'" -namespace "root/cimv2/TerminalServices" | Select NetworkAdapterLanaID,NetworkAdapterName)
+	Write-Host ("New network adapter ID for RDP: " + $updatedSettings.NetworkAdapterLanaID)
+	Write-Host ("New network adapter name for RDP: " + $updatedSettings.NetworkAdapterName)
+	
+	Write-Host "Restarting Remote Desktop Services in 5 seconds"
+	Start-Sleep -Seconds 5
+	Restart-Service -Force -DisplayName "Remote Desktop Services"
+}
+Else {
+	Write-Host "Tailscale adapter was not found, so no changes were made"
+}
